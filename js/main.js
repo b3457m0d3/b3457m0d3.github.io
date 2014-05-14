e8d44bb32e35a25006d443d97370de6d9ddcc622
Jessie Taylor <b3457m0d3@interr0bang.net>
2014-05-14 02:50:43 -0400

    boom

--- js/main.js
+++ js/main.js
@@ -16,8 +16,11 @@ $(function() {
             max_rows: 1,
             duration: 5500,
             pauseOnHover: 1,
-            hasMoved: function(){
-                //update the well with relevant information based on the ticker item
+            hasMoved: function() {
+                $('#sticker').fadeOut(200, function(){
+                    $('#sticker').html($('#ticker li:first').data('legend'));
+                    $(this).fadeIn(400);
+                });
             },
             pause: function(){
                 //flash the pause icon
