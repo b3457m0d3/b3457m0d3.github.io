6c60e961ae4f4e196c58f65737442cb6bbf924b1
Jessie Taylor <b3457m0d3@interr0bang.net>
2014-05-14 06:09:19 -0400

    hasMoved

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ $(function() {
             max_rows: 1,
             duration: 5500,
             pauseOnHover: 1,
-            movingUp: function() {
+            hasMoved: function() {
                 $('#sticker').fadeOut(200, function(){
                     $('#sticker').html($('#ticker li:first').data('legend'));
                     $(this).fadeIn(400);
