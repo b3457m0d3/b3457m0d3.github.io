d9736e1e8d096ba3b3e281745c4f8f362b581f3e
Jessie Taylor <b3457m0d3@interr0bang.net>
2014-06-08 11:40:29 -0400

    boom

--- js/main.js
+++ js/main.js
@@ -63,9 +63,13 @@ $(function() {
                     
                     $('#welcomeText').html('Hi').addClass('text-primary');
                     $('#nameText').html($('#contactForm').data("name").capitalize()).addClass('text-info');
-                    $('#nameText').parent().find('small').html('You can call me <span class="typist"></span>').before('<br/>');
+                    $('#nameText').parent().find('small').html('You can call me <span class="typist sp-elite blue"></span>').before('<br/>');
                     
-                    $('.typist').typist({'fontFamily':"Special Elite",'textColor':"#0f86ce"}).typist('type', 'b3457m0d3'); 
+                    $('.typist').typed({
+                        strings: ["b3457m0d3"],
+                        typeSpeed: 0
+                    });
+                    //.typist({'fontFamily':"Special Elite",'textColor':"#0f86ce"}).typist('type', 'b3457m0d3'); 
 
 
                     $icon.fadeOut().removeClass('fa-spinner fa-spin').addClass('fa-check').fadeIn();
