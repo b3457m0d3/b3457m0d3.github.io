36b9b030040b9ec3290d6723f3d200420fd23bdf
Jessie Taylor <b3457m0d3@interr0bang.net>
2014-06-08 12:07:30 -0400

    boom

--- js/main.js
+++ js/main.js
@@ -74,7 +74,8 @@ $(function() {
                     
                     $('#nameGroup').animate({ width: [ 0, "swing" ], opacity: 0 }, 3500, "linear",function(){ $('#intro').fadeIn('slow').removeClass('hide'); });
                     
-                    $('#rightPanel').animate({ width: [ 350, "swing" ], opacity: 1 }, 1000, "linear").removeClass('hide');
+                    $('#rightPanel').animate({ width: [ 350, "swing" ], opacity: 1 }, 1000, "linear");
+                    $('#to2').fadeIn().removeClass('hide');
                     
                 } else {
                     $icon.fadeOut().removeClass('fa-spinner fa-spin').addClass('fa-ban').fadeIn();
