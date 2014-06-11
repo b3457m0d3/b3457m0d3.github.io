1418b6f1df27bcce7a56f3ee4cf3c48acb4efe36
Jessie Taylor <b3457m0d3@interr0bang.net>
2014-06-10 23:01:31 -0400

    boom

--- js/main.js
+++ js/main.js
@@ -74,7 +74,7 @@ $(function() {
                     
                     $('#nameGroup').animate({ width: [ 0, "swing" ], opacity: 0 }, 3500, "linear",function(){ $('#intro').fadeIn('slow').removeClass('hide'); });
                     
-                    $('#rightPanel').animate({ width: [ 350, "swing" ], opacity: 1 }, 2000, "linear", function(){ $('#to2').fadeIn("slow").removeClass('hide'); });
+                    $('#rightPanel').removeClass('hide').animate({ width: [ 350, "swing" ] }, 2000, "linear", function(){ $('#to2').fadeIn("slow").removeClass('hide'); });
                     
                     
                 } else {
